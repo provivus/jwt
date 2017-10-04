23607c5b1f0ab680ffb81a39ff9fbd12b02ba1c4
Johan Sellström <johan.sellstrom@combient.com>
2017-10-04 14:14:37 +0200

    carthage

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "JWT",
     dependencies: [
-        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2),
+        .Package(url: "https://github.com/provivus/json.git", majorVersion: 2),
     ],
     exclude: [
         "Playground"
